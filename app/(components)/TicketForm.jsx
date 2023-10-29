518319f8b5a2e8d60c03e468740ca09421daf86b
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-10-29 09:07:49 -0400

    feat: add update functionality

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -2,7 +2,9 @@
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
-const TicketForm = () => {
+const TicketForm = ({ ticket }) => {
+  const EDITMODE = ticket._id === "new" ? false : true;
+
   const router = useRouter();
 
   const handleChange = (e) => {
@@ -18,18 +20,33 @@ const TicketForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch("/api/Tickets", {
-      method: "POST",
-      body: JSON.stringify({ formData }),
-      "content-type": "application/json",
-    });
+    if (EDITMODE) {
+      const res = await fetch(`/api/Tickets/${ticket._id}`, {
+        method: "PUT",
+        body: JSON.stringify({ formData }),
+        "content-type": "application/json",
+      });
 
-    if (!res.ok) {
-      throw new Error("Faild to create Ticket.");
+      if (!res.ok) {
+        throw new Error("Faild to update Ticket.");
+      }
+
+      router.refresh();
+      router.push("/");
+    } else {
+      const res = await fetch("/api/Tickets", {
+        method: "POST",
+        body: JSON.stringify({ formData }),
+        "content-type": "application/json",
+      });
+
+      if (!res.ok) {
+        throw new Error("Faild to create Ticket.");
+      }
+
+      router.refresh();
+      router.push("/");
     }
-
-    router.refresh();
-    router.push("/");
   };
 
   const startingTicketData = {
@@ -41,6 +58,15 @@ const TicketForm = () => {
     category: "Hardware Problem",
   };
 
+  if (EDITMODE) {
+    startingTicketData["title"] = ticket.title;
+    startingTicketData["description"] = ticket.description;
+    startingTicketData["priority"] = ticket.priority;
+    startingTicketData["progress"] = ticket.progress;
+    startingTicketData["status"] = ticket.status;
+    startingTicketData["category"] = ticket.category;
+  }
+
   const [formData, setFormData] = useState(startingTicketData);
 
   return (
@@ -50,7 +76,7 @@ const TicketForm = () => {
         method="post"
         onSubmit={handleSubmit}
       >
-        <h3>Create Your Ticket</h3>
+        <h3>{EDITMODE ? "Update Your Ticket" : "Create Your Ticket"}</h3>
 
         <label>Title</label>
         <input
@@ -154,7 +180,11 @@ const TicketForm = () => {
           <option value={"done"}>Done</option>
         </select>
 
-        <input type="submit" className="btn" value={"Create Ticket"} />
+        <input
+          type="submit"
+          className="btn"
+          value={EDITMODE ? "Update Ticket" : "Create Ticket"}
+        />
       </form>
     </div>
   );
