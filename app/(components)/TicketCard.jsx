8790aa52789dea178c2354a928a7a22914d3aa33
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-10-29 00:44:06 -0400

    feat: add PriorityDisplay functionality

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -3,17 +3,17 @@ import PriorityDisplay from "./PriorityDisplay";
 import ProgressDisplay from "./ProgressDisplay";
 import StatusDisplay from "./StatusDisplay";
 
-const TicketCard = () => {
+const TicketCard = ({ticket}) => {
   return (
     <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
       <div className="flex mb-3">
-        <PriorityDisplay />
+        <PriorityDisplay priority={ticket.priority} />
         <div className="ml-auto">
           <DeleteBlock />
         </div>
       </div>
       <h4>Ticket Title</h4>
-      <hr className="h-px border-0 bg-page mb-2" />
+      <hr className="h-ix border-0 bg-page mb-2" />
       <p className="whitespace-pre-wrap">
         this is the ticket description! Please do this ticket
       </p>
